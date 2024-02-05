d66d000eaa252bf0b1e81ea60b6a73496a1f0ad5
abdoshehab7 <abdoshehab7@gmail.com>
2024-02-06 01:43:15 +0200

    Feat: Added PopularDestinationsView, PopularRestaurantsView and TrendingCreatorsView.

--- Travel Discovery/ContentView.swift
+++ Travel Discovery/ContentView.swift
@@ -12,6 +12,12 @@ struct ContentView: View {
         NavigationView {
             ScrollView {
                 DiscoverCategoriesView()
+                
+                PopularDestinationsView()
+                
+                PopularRestaurantsView()
+                
+                TrendingCreatorsView()
             }
             .navigationTitle("Discover")
         }
@@ -43,6 +49,96 @@ struct DiscoverCategoriesView: View {
             .padding(.horizontal)
             .scrollIndicators(.hidden)
             
+        }
+    }
+}
+
+struct PopularDestinationsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular Destination")
+                    .font(.system(size: 14, weight: .bold, design: .default))
+                Spacer()
+                Text("See All")
+                    .font(.system(size: 14, weight: .bold, design: .default))
+            }
+            .padding()
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8, content: {
+                    ForEach(0...5, id: \.self) { _ in
+                        Spacer()
+                            .frame(width: 125, height: 150)
+                            .background(.gray)
+                            .cornerRadius(5)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2.0)
+                            .padding(.bottom)
+                    }
+                    
+                })
+                .padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct PopularRestaurantsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Popular Places To Eat")
+                    .font(.system(size: 14, weight: .bold, design: .default))
+                Spacer()
+                Text("See All")
+                    .font(.system(size: 14, weight: .bold, design: .default))
+            }
+            .padding()
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8, content: {
+                    ForEach(0...5, id: \.self) { _ in
+                        Spacer()
+                            .frame(width: 200, height: 64)
+                            .background(.gray)
+                            .cornerRadius(5)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2.0)
+                            .padding(.bottom)
+                    }
+                    
+                })
+                .padding(.horizontal)
+            }
+        }
+    }
+}
+
+struct TrendingCreatorsView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Trending Creators")
+                    .font(.system(size: 14, weight: .bold, design: .default))
+                Spacer()
+                Text("See All")
+                    .font(.system(size: 14, weight: .bold, design: .default))
+            }
+            .padding()
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 8, content: {
+                    ForEach(0...15, id: \.self) { _ in
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2.0)
+                            .padding(.bottom)
+                    }
+                    
+                })
+                .padding(.horizontal)
+            }
         }
     }
 }
