596a5cd8dd723922747c7baab416ec80d8abcfa2
abdoshehab7 <abdoshehab7@gmail.com>
2024-02-05 18:53:44 +0200

    Initial Commit

---
+++ Travel Discovery/Travel_DiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Travel_DiscoveryApp.swift
+//  Travel Discovery
+//
+//  Created by Abdelrahman Shehab on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Travel_DiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
