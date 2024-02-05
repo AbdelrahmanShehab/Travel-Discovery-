8031f8fa007cd2c35c844866678615c1a71cbd8e
abdoshehab7 <abdoshehab7@gmail.com>
2024-02-05 19:10:57 +0200

    Feat: Add Skeleton DiscoverCategoriesView

--- Travel Discovery/ContentView.swift
+++ Travel Discovery/ContentView.swift
@@ -9,16 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                DiscoverCategoriesView()
+            }
+            .navigationTitle("Discover")
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct DiscoverCategoriesView: View {
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 8) {
+                ForEach(0...4, id: \.self) { _ in
+                    VStack(spacing: 8, content: {
+                        Spacer()
+                            .frame(width: 50, height: 50)
+                            .background(.gray)
+                            .cornerRadius(.infinity)
+                            .shadow(color: .gray, radius: 4, x: 0.0, y: 2.0)
+                        Text("Art")
+                            .font(.system(size: 12, weight: .semibold, design: .default))
+                        
+                    })
+                }
+                
+            }
+            .padding(.horizontal)
+            .scrollIndicators(.hidden)
+            
+        }
+    }
 }
